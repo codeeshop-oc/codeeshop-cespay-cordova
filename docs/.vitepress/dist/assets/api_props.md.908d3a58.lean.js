bcfc7dad3957f6fba0dcf8aa1262f3347b28fcab
codeeshop-oc <anantnegi8@gmail.com>
2023-10-26 12:33:38 +0530

    added dist folder

---
+++ docs/.vitepress/dist/assets/api_props.md.908d3a58.lean.js
@@ -0,0 +1,1 @@
+import{_ as t,o as r,c as a,Q as e}from"./chunks/framework.edbfdbf1.js";const b=JSON.parse('{"title":"Props","description":"","frontmatter":{},"headers":[],"relativePath":"api/props.md","filePath":"api/props.md"}'),d={name:"api/props.md"},o=e("",5),s=[o];function p(n,i,l,c,_,h){return r(),a("div",null,s)}const P=t(d,[["render",p]]);export{b as __pageData,P as default};
